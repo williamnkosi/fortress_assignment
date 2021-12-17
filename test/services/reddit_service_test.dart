bb59d67ef2533e720a920f4e6631d340fb6c04a1
williamnkosi <william.dev.acc@gmail.com>
2021-12-17 11:51:31 -0600

    Added the mockitoTail mock paackage, created test files for reddit_service and home_page_view_model

---
+++ test/services/reddit_service_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fortress_assignment/models/topic.dart';
+import 'package:fortress_assignment/services/reddit_service.dart';
+import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
+import 'mock_topics_json.dart';
+
+class MockHttp extends Mock implements http.Client {}
+
+void main() {
+  test('reddit service ...', () async {
+    final client = MockHttp();
+    var url = Uri.https("www.reddit.com", "/r/programmerhumor/top.json");
+    final redditService = RedditService();
+    when(() => client.get(url))
+        .thenAnswer((_) => Future.value(http.Response(jsonData, 200)));
+    expect(await redditService.getTopTopics(), isA<List<Topic>>());
+  });
+}
